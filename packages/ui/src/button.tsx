2700e87c8a5dba5d828419f2fb2a44e441493e8a
Tommaso <65722261+TommasoTate@users.noreply.github.com>
2026-08-05 08:02:44 +0200

    added biome

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 interface ButtonProps {
+  appName: string;
   children: ReactNode;
   className?: string;
-  appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
-};
+export const Button = ({ children, className, appName }: ButtonProps) => (
+  <button
+    className={className}
+    onClick={() => alert(`Hello from your ${appName} app!`)}
+  >
+    {children}
+  </button>
+);
